288a843fc509a38b91c3c52fd80556df8f427160
krdheeraj51 <krdheeraj51@gmail.com>
2022-10-31 17:53:10 +0530

    make changes in script.

--- lib/cdk_learning-stack.ts
+++ lib/cdk_learning-stack.ts
@@ -1,16 +1,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class CdkLearningStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLearningQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new s3.Bucket(this, 'myStorageBucket', {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
   }
 }
